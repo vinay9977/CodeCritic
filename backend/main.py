391e60ac6dfc64d7976752facf8b600132fbeee7
Vinay Korrapati <korrv01@pfw.edu>
2025-10-25 10:09:58 -0400

    added vercel url

--- backend/main.py
+++ backend/main.py
@@ -30,6 +30,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://code-critic-fn81lq7eq-vinays-projects-c22223e9.vercel.app" ,
     ],
     allow_credentials=True,
     allow_methods=["*"],
